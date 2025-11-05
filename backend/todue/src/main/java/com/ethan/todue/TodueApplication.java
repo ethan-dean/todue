a5a6bd8b31ea123002adecec98af4c8c64774121
Ethan Dean <ethandean7@gmail.com>
2025-11-05 15:16:25 -0500

    feat: initial commit

---
+++ backend/todue/src/main/java/com/ethan/todue/TodueApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.todue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodueApplication.class, args);
+	}
+
+}
---
+++ backend/todue/src/test/java/com/ethan/todue/TodueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.todue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
